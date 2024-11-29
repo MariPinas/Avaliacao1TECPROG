2caaf9d19b18d974bca366eec8fedbdbb786e05a
MariPinas <marianapls2003@gmail.com>
2024-11-28 22:14:33 -0300

    :sparkles: -ultimas alteracoes Mariana- adicionado exemplo de CRUD no Program.cs

--- Program.cs
+++ Program.cs
@@ -4,16 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Avaliacao1{
-    internal class Program{
-        static void Main(string[] args){
+namespace Avaliacao1 {
+    internal class Program {
+        static void Main(string[] args) {
             DAOCliente daoCliente = new DAOCliente();
 
+
+            //create cliente
             Cliente cliente1 = new Cliente("Mariana", 18, "1234532");
-
             daoCliente.create(cliente1);
-
             daoCliente.getAll();
+            //update cliente
+            Cliente clienteUP = new Cliente("Mariana2", 19, "123456");
+            daoCliente.update(clienteUP);
+            //delete cliente
+            Cliente clienteD = daoCliente.read(1);
+
+            if (daoCliente.delete(clienteD.id))
+                Console.WriteLine("Exclusao com sucesso");
+            else
+                Console.WriteLine("Exclusao nao realizada");
+            // get all cliente
+            daoCliente.getAll();
+
+            // Básicamente é esse o formato de CRUD para todas as classes,
+            // recomendo ir comentando e descomentando e ir rodando parte por parte e não tudo de uma vez
 
         }
     }
